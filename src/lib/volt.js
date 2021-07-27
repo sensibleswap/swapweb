7f5dcef26a71a9566524a617f89b569fb64ba5ee
sunlixu <sunlixu@163.com>
2021-07-27 15:40:00 +0800

    volt钱包切换到线上环境

--- src/lib/volt.js
+++ src/lib/volt.js
@@ -1,11 +1,20 @@
 import { Bsv } from '@volt-wallet/sdk';
 
+// const bsv = new Bsv({
+//   iframeUrl: 'http://47.108.83.26:9000/iframe/', // iframe 页面地址, 测试时使用这个地址
+//   popupUrl: 'http://47.108.83.26:9000/popup/', // popup 页面地址, 测试时使用这个地址
+//   apiPrefix: {
+//     tls: false, // 后端接口是否 https
+//     endpoint: '47.108.83.26:7001', // 后端接口地址, 测试时使用这个地址
+//   },
+// });
+
 const bsv = new Bsv({
-  iframeUrl: 'http://47.108.83.26:9000/iframe/', // iframe 页面地址, 测试时使用这个地址
-  popupUrl: 'http://47.108.83.26:9000/popup/', // popup 页面地址, 测试时使用这个地址
+  iframeUrl: 'https://sdkpage.volt.id/iframe/',
+  popupUrl: 'https://sdkpage.volt.id/popup/',
   apiPrefix: {
-    tls: false, // 后端接口是否 https
-    endpoint: '47.108.83.26:7001', // 后端接口地址, 测试时使用这个地址
+    tls: true,
+    endpoint: 'volt.id',
   },
 });
 
