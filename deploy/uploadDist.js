375ec2ffde86193199aa89d760a7547c7e2112a9
klouskingsley <harry_tse@163.com>
2021-09-16 23:42:28 +0800

    test secret

--- deploy/uploadDist.js
+++ deploy/uploadDist.js
@@ -4,12 +4,12 @@ const readdir = require('readdir');
 
 const gitCommitHash = process.env.GITHUB_SHA.substr(0, 7);
 
-console.log(process.env.GITHUB_SHA, process.env.ALIYUN_OSS_SECRET);
+// console.log(process.env.GITHUB_SHA, process.env.ALIYUN_OSS_SECRET);
 
 const client = new OSS({
   region: 'oss-cn-hongkong',
   accessKeyId: 'LTAI5tL3nUgFyKsgsXTh3JkM',
-  accessKeySecret: process.env.ALIYUN_OSS_SECRET,
+  accessKeySecret: process.env.aliyun_oss_secret,
   bucket: 'volt',
 });
 
