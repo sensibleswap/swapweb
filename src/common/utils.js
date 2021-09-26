5ae2ccb352f0eb2e589a7dcbf49dc6407933dbcd
sunlixu <sunlixu@163.com>
2021-09-26 15:24:48 +0800

    避免显示科学计数

--- src/common/utils.js
+++ src/common/utils.js
@@ -200,7 +200,12 @@ export function jc() {
 export const formatSat = (value, dec = 8) => {
   if (!value) return 0;
 
-  return BigNumber(value).div(Math.pow(10, dec)).toString();
+  const v = BigNumber(value).div(Math.pow(10, dec));
+  const arr = v.toString().split('.');
+  if (v.toString().indexOf('e') > -1 || (arr[1] && arr[1].length > dec)) {
+    return BigNumber(v).toFixed(dec);
+  }
+  return v.toString();
 };
 
 export function sleep(ms) {
